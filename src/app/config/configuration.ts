1c5eb069e53530953bfa87a3f242cd772d4a2d7c
Than Htet Aung <thanhtetaung87@gmail.com>
2021-03-05 18:27:06 +0900

    updated wrong configuration

--- src/app/config/configuration.ts
+++ src/app/config/configuration.ts
@@ -1,6 +1,6 @@
 export const configuration = {
-  baseUrl: 'http://localhost:8080/v1/',
-  // baseUrl: 'http://building-service-2058551938.ap-northeast-1.elb.amazonaws.com/v1/',
+  // baseUrl: 'http://localhost:8080/v1/',
+  baseUrl: 'http://building-service-2058551938.ap-northeast-1.elb.amazonaws.com/v1/',
   contentType: 'application/json',
   accept: 'application/json'
 };
