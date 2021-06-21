339e64cfd14a718739c2e79338544564d60e715b
hirao-k <nuit.noir.8@gmail.com>
2021-06-21 22:06:09 +0900

    Add minFloorAreaRatioLimit

--- src/app/models/building-site.ts
+++ src/app/models/building-site.ts
@@ -1,6 +1,7 @@
 export class BuildingSite {
   siteArea!: number;
   floorAreaRatioLimit!: number;
+  minFloorAreaRatioLimit!: number;
   floorAreaRatioLegalMultiplier!: number;
   floorAreaRatioLimitAddingValue!: number;
 }
